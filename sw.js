6ecbf614da5b175937e7b2457aed5705b96232d2
simpartpqnurulfalah <simpartpqnurulfalah@gmail.com>
2026-04-17 10:43:11 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,1 @@
-// 1. Inisialisasi OneSignal & Paksa Munculkan Pop-up Izin
-window.OneSignalDeferred = window.OneSignalDeferred || [];
-OneSignalDeferred.push(async function(OneSignal) {
-    await OneSignal.init({
-        appId: "8975b58d-f7bd-4217-91a0-acb804361bb5",
-        serviceWorkerPath: "sw.js", // Beri tahu OneSignal nama file Mas
-        serviceWorkerParam: { scope: "/web/" }, // Beri tahu letak foldernya
-        notifyButton: {
-            enable: true, // Memunculkan ikon lonceng
-        }
-    });
-    
-    // Paksa pop-up izin muncul dari atas
-    OneSignal.Slidedown.promptPush();
-});
+importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
